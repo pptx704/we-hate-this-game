5b4e89a9acc7e116e4980b621a10bbe12aff6f63
pptx704 <rafeedm.bhuiyan@gmail.com>
2022-07-04 10:36:56 +0300

    Final implementation

--- app/Level0.hs
+++ app/Level0.hs
@@ -2,13 +2,27 @@ module Level0 where
 import Graphics.Gloss
 import Interactions
 import Screens
+import Assets
 import WeHateThisGame
 import Graphics.Gloss.Interface.IO.Game
 
 -- | No level specific drawing
 drawLv0 :: State Block -> Picture
-drawLv0 _ = blank
-
+drawLv0 (State t _ _ _ _ gs) = case gs of
+    Resumed -> color (getForegroundColor t) 
+        $ pictures [themeChange, pause, resume, move]
+    _ -> blank
+    where
+        themeChange = translate 300 (-200) $
+            scaledText "Press Shift + Space to Change Theme"
+        pause = translate 300 (-300) $
+            scaledText "Press Shift + UpArrow to Pause"
+        resume = translate 300 (-400) $
+            scaledText "Press Shift + DownArrow to Resume"
+        move = translate 300 (-500) $
+            scaledText "Use Arrow Keys to Move and Space to Jump"
+        scaledText = scale 0.3 0.3 . Text
+        
 -- | Handleworld is basic
 handleWorld :: Event -> State Block -> State Block
 handleWorld (EventKey (SpecialKey k) pos sp _) state
